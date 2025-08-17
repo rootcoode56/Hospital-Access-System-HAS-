5c2feeee07b5017c389103042787a426e6dd80b7
noman01234 <ragib.shahriar01@northsouth.edu>
2025-08-17 12:49:58 +0600

    Bot

---
+++ lib/Services/api_service.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+import 'package:has/constant/api_constant.dart';
+import 'package:http/http.dart' as http;
+
+class GoogleApiService {
+  static String apiKey = ApiConstant.apiKey;
+  static String baseUrl = ApiConstant.apiUrl;
+
+  static Future<String> getApiResponse(String message) async {
+    try {
+      final response = await http.post(
+        Uri.parse("$baseUrl?key=$apiKey"),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          "contents": [
+            {
+              "parts": [
+                {"text": message},
+              ],
+            },
+          ],
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+
+        //chch if response has condidates and content
+        // Check if response has "candidates" and "content"
+        if (data.containsKey("candidates") && data["candidates"].isNotEmpty) {
+          var firstCandidate = data["candidates"][0];
+
+          if (firstCandidate.containsKey("content") &&
+              firstCandidate["content"].containsKey("parts") &&
+              firstCandidate["content"]["parts"].isNotEmpty) {
+            return firstCandidate["content"]["parts"][0]["text"] ??
+                "AI response was empty.";
+          }
+        }
+        return "AI did not return any content.";
+      } else {
+        return "Error: ${response.statusCode} - ${response.body}";
+      }
+    } catch (e) {
+      print("Error=> $e");
+      return "Error: $e";
+    }
+  }
+}
